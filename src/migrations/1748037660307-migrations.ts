c32880a7479ea122816c743627fa4fb8ce04c4eb
rishavrapta <rishavrapta@rishavraptas-MacBook-Air.local>
2025-05-24 02:44:21 +0300

    Added migrations format

--- src/migrations/1748037660307-migrations.ts
+++ src/migrations/1748037660307-migrations.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class RenameMenuSectionsToMenuCategories1748035406085 implements MigrationInterface {
+export class RenameMenuSectionsToMenuCategories1748035406085
+  implements MigrationInterface
+{
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `DROP TABLE IF EXISTS "menu_categories"`,
-    );
+    await queryRunner.query(`DROP TABLE IF EXISTS "menu_categories"`);
     await queryRunner.query(
       `ALTER TABLE "menu_sections" RENAME TO "menu_categories"`,
     );
