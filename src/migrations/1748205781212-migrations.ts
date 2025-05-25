ed19f0267c8e1aef6c3cb3c323ad67f608b59c8b
rishavrapta <rishavrapta@rishavraptas-MacBook-Air.local>
2025-05-25 23:49:20 +0300

    Fixed formatting:

--- src/migrations/1748205781212-migrations.ts
+++ src/migrations/1748205781212-migrations.ts
@@ -1,21 +1,25 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Migrations1748205781212 implements MigrationInterface {
-    name = 'Migrations1748205781212'
+  name = "Migrations1748205781212";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-         await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
       UPDATE roles SET role_id = 3 WHERE role_id = 0
     `);
-        await queryRunner.query(`ALTER TABLE "teams" DROP COLUMN "status"`);
-await queryRunner.query(`ALTER TABLE "teams" ADD "name" text`);
-await queryRunner.query(`UPDATE "teams" SET "name" = 'Unknown' WHERE "name" IS NULL`);
-await queryRunner.query(`ALTER TABLE "teams" ALTER COLUMN "name" SET NOT NULL`); }
+    await queryRunner.query(`ALTER TABLE "teams" DROP COLUMN "status"`);
+    await queryRunner.query(`ALTER TABLE "teams" ADD "name" text`);
+    await queryRunner.query(
+      `UPDATE "teams" SET "name" = 'Unknown' WHERE "name" IS NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "teams" ALTER COLUMN "name" SET NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "teams" DROP COLUMN "is_available"`);
-        await queryRunner.query(`ALTER TABLE "teams" DROP COLUMN "name"`);
-        await queryRunner.query(`ALTER TABLE "teams" ADD "status" text`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "teams" DROP COLUMN "is_available"`);
+    await queryRunner.query(`ALTER TABLE "teams" DROP COLUMN "name"`);
+    await queryRunner.query(`ALTER TABLE "teams" ADD "status" text`);
+  }
 }
