4d5a062d4bb17229d5476a295c712f4c6e3c0b4a
rishavrapta <rishavrapta@rishavraptas-MacBook-Air.local>
2025-05-27 00:01:27 +0300

    Fixed formatting

--- src/migrations/1748292630027-migrations.ts
+++ src/migrations/1748292630027-migrations.ts
@@ -1,14 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Migrations1748292630027 implements MigrationInterface {
-    name = 'Migrations1748292630027'
+  name = "Migrations1748292630027";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "teams" ADD "is_available" boolean NOT NULL DEFAULT true`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "teams" ADD "is_available" boolean NOT NULL DEFAULT true`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "teams" DROP COLUMN "is_available"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "teams" DROP COLUMN "is_available"`);
+  }
 }
