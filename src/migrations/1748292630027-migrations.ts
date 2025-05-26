d988de402f3dc2a48b32d4a61f244408a74f1205
rishavrapta <rishavrapta@rishavraptas-MacBook-Air.local>
2025-05-26 23:52:06 +0300

    Added is available column for team

---
+++ src/migrations/1748292630027-migrations.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migrations1748292630027 implements MigrationInterface {
+    name = 'Migrations1748292630027'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "teams" ADD "is_available" boolean NOT NULL DEFAULT true`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "teams" DROP COLUMN "is_available"`);
+    }
+
+}
